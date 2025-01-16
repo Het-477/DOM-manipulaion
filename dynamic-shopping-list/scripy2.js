185f7e01dec2bf7db715a03df9f0fc5ae5561c33
Het Gurjar <130950604+Het-477@users.noreply.github.com>
2025-01-16 12:20:06 +0530

    chore: add text & btn to list item and then to shopping list

--- dynamic-shopping-list/scripy2.js
+++ dynamic-shopping-list/scripy2.js
@@ -2,9 +2,7 @@ const shoppingList = document.querySelector("ul");
 const input = document.querySelector("input");
 const addButton = document.querySelector("button");
 
-addButton.addEventListener("click", () => {
-
-});
+addButton.addEventListener("click", addListItem);
 
 function addListItem() {
     // creating elements to add inside our shopping list 
@@ -17,5 +15,16 @@ function addListItem() {
     if (input.value === '') return;
     input.value = '';
 
+    // adding span and delete btn into list item
+    listItem.appendChild(listText);
+    listText.innerText = inputText;
+    listItem.appendChild(itemDeleteBtn);
+    itemDeleteBtn.innerText = "Delete";
+    itemDeleteBtn.addEventListener("click", () => { // for a functioning delete button 
+        shoppingList.removeChild(listItem);
+    })
+
+    // adding list item into the shopping list.
+    shoppingList.appendChild(listItem);
 
 }
